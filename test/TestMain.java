b77ad8a59252aa1ee843013c0ec80535b54405e0
ShafigullinIK <shafigullin.ik@gmail.com>
2019-12-09 13:31:47 +0300

    Lesson end commit

--- test/TestMain.java
+++ test/TestMain.java
@@ -4,10 +4,5 @@ import org.junit.Test;
 
 public class TestMain {
 
-    @Test
-    public void testSquare(){
-        Assert.assertEquals(4, Main.square(2));
-        Assert.assertEquals(4, Main.square(-2));
-        Assert.assertEquals(0, Main.square(0));
-    }
+
 }
